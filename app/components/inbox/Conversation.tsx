fd0135a7a2084361e4e320d0ee38da057ef61d4b
Aliix <loveitza50@gmail.com>
2025-02-27 10:45:58 -0600

    05 My properties and Inbox page

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,9 @@
+const Conversation = () => {
+    return(
+        <div className="px-6 py-4 cursor-pointer border border-gray-400 rounded-xl">
+            <p className="mb-6 text-xl">Jen</p>
+            <p className="text-pink-800">Ver</p>
+        </div>
+    )
+}
+export default Conversation
---
+++ app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,31 @@
+'use client';
+import CustomButton from "../forms/CustomButton"
+
+const ConversationDetail = () => {
+    return (
+        <>
+            <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+                <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-400">
+                    <p className="font-mono text-white">Jen</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Cupiditate obcaecati nam architecto in quibusdam suscipit officia alias,
+                        labore aliquid ex repudiandae atque maiores magni illum exercitationem ducimus dolorem aut? Ut?</p>
+                </div>
+
+                <div className="w-[80%] ml-[20%] py-4 px-6 rounded-xl bg-gray-400">
+                    <p className="font-mono text-white">Yop</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Cupiditate obcaecati nam architecto in quibusdam suscipit officia alias,
+                        labore aliquid ex repudiandae atque maiores magni illum exercitationem ducimus dolorem aut? Ut?</p>
+                </div>
+            </div>
+            <div className="mt-4 py-4 px-6 flex border border-gray-400 space-x-4 rounded-xl decoration">
+                <input type="text" placeholder="Escribe tu mensaje..." className="w-full p-2 -bg-gray-300 rounded-xl" />
+                <CustomButton label='Send' onClick={() => console.log('Clicked')} className="w-[100px]" />
+                
+            </div>
+
+        </>
+    )
+}
+export default ConversationDetail
