1c03f780c61a0ed94133b4876055d042f6adeca1
Aliix <loveitza50@gmail.com>
2025-03-11 11:43:32 -0600

    07 Properties and list

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image"
-const PropertyListItem = () => {
+import { PropertyType } from "./PropertyList"
+
+interface PropertyProps {
+    property: PropertyType
+}
+
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property
+}) => {
     return(
         <div className="cursor-pointer">
             <div className="relative overflow-hidden aspect-square rounded-xl">
-                <Image fill src='/alberca_1.jpeg' sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+                <Image fill src={property.image_url} sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                 className="hover:scale-110 object-cover transition h-full w-full"
                 alt="Casa con Alberca"/>
             </div>
             <div className="mt-2">
-                <p className="text-lg font-mono">Property name</p>
+                <p className="text-lg font-mono">{property.title}</p>
             </div>
             <div className="mt-2">
-                <p className="text-sm text-gray-500"><strong>$1000 la noche</strong></p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong></p>
             </div>
         </div>
     )
